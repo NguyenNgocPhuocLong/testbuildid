ddba317ed0873331ad2d7b8c99199570050c833c
NgocLong <nguyenngocphuoclong1994@gmail.com>
2023-10-22 12:16:32 +0700

    update lib

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import {  useVersionDetectHook } from 'vdtest';
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const {loading } = useVersionDetectHook({
+    nameArg:"test"
+  });
+  console.log('loading', loading);
 
   return (
     <>
